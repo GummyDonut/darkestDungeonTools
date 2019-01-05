4022bac4dc77a73b3199eb52793ac0178fd847ce
lantern77 <peter_hoang2@hotmail.com>
2019-01-05 11:20:42 -0500

    Finally got eslint to work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,39 @@
 module.exports = {
-    "extends": "standard"
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ]
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
-const App = () => {
+const App = () => 
+    <img src="https://d1u5p3l4wpay3k.cloudfront.net/darkestdungeon_gamepedia/thumb/e/e7/Discarded_Pack.png/180px-Discarded_Pack.png?version=9e1fe60faa30286183f94e4d3d45198f"></img>
 
-}
+
+export default App
